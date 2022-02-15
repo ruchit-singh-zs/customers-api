8f6b6186f33068825fd936b802dca51bdf2daeca
ruchit-singh-zs <ruchit.singh@zopsmart.com>
2022-02-15 20:32:09 +0530

    refactored routes

--- main.go
+++ main.go
@@ -1,36 +1,42 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-
-	"customerApi/drivers"
-	handlers "customerApi/handlers/customer"
-	"customerApi/middleware"
-	stores "customerApi/stores/customer"
+	handlers "customers-api/handlers/customer"
+	stores "customers-api/stores/customer"
+	"developer.zopsmart.com/go/gofr/pkg/gofr"
 )
 
+//func main() {
+//	db, err := drivers.ConnectToSQL()
+//	if err != nil {
+//		log.Fatalf("FATAL, Can't Connect to database")
+//	}
+//	defer db.Close()
+//	s := stores.New(db)
+//	h := handlers.New(s)
+//
+//	r := mux.NewRouter()
+//	r.HandleFunc("/customer/{id}", h.GetByID).Methods(http.MethodGet)
+//	r.HandleFunc("/customer", h.Create).Methods(http.MethodPost)
+//	r.HandleFunc("/customer/delete/{id}", h.DeleteByID).Methods(http.MethodDelete)
+//	r.HandleFunc("/customer/update/{id}", h.UpdateByID).Methods(http.MethodPut)
+//
+//	r.Use(middleware.SetContentType)
+//
+//	err = http.ListenAndServe(":8080", r)
+//	if err != nil {
+//		log.Println("Cant Connect!")
+//	}
+//}
+
 func main() {
-	db, err := drivers.ConnectToSQL()
-	if err != nil {
-		log.Fatalf("FATAL, Can't Connect to database")
-	}
-	defer db.Close()
-	s := stores.New(db)
+	app := gofr.New()
+	s := stores.New()
 	h := handlers.New(s)
-
-	r := mux.NewRouter()
-	r.HandleFunc("/customer/{id}", h.GetByID).Methods(http.MethodGet)
-	r.HandleFunc("/customer", h.Create).Methods(http.MethodPost)
-	r.HandleFunc("/customer/delete/{id}", h.DeleteByID).Methods(http.MethodDelete)
-	r.HandleFunc("/customer/update/{id}", h.UpdateByID).Methods(http.MethodPut)
-
-	r.Use(middleware.SetContentType)
-
-	err = http.ListenAndServe(":8080", r)
-	if err != nil {
-		log.Println("Cant Connect!")
-	}
+	app.Server.ValidateHeaders = false
+	app.GET("/customer/{id}", h.GetByID)
+	app.POST("/customer", h.Create)
+	app.PUT("/customer/update/{id}", h.UpdateByID)
+	app.DELETE("/customer/delete/{id}", h.DeleteByID)
+	app.Start()
 }
