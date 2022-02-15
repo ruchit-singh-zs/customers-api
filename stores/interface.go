130410c59433bb39768b9fe1f9749fe4f5e504c1
ruchit-singh-zs <ruchit.singh@zopsmart.com>
2022-02-15 20:32:56 +0530

    refactored interface methods

--- stores/interface.go
+++ stores/interface.go
@@ -1,10 +1,13 @@
 package stores
 
-import "customerApi/models"
+import (
+	"customers-api/models"
+	"developer.zopsmart.com/go/gofr/pkg/gofr"
+)
 
 type Customer interface {
-	Create(c models.Customer) error
-	Get(id int) (models.Customer, error)
-	Update(id int, c models.Customer) error
-	Delete(id int) error
+	Create(ctx *gofr.Context, c models.Customer) (models.Customer, error)
+	GetByID(ctx *gofr.Context, id string) (c models.Customer, e error)
+	UpdateByID(ctx *gofr.Context, id string, c models.Customer) error
+	DeleteByID(ctx *gofr.Context, id string) (models.Customer, error)
 }
