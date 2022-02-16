0a8897550bd8d14a510bbad262d9dce0234da2b6
ruchit-singh-zs <ruchit.singh@zopsmart.com>
2022-02-16 15:16:23 +0530

    refactored main file and removed commented code

--- main.go
+++ main.go
@@ -6,29 +6,6 @@ import (
 	"developer.zopsmart.com/go/gofr/pkg/gofr"
 )
 
-//func main() {
-//	db, err := drivers.ConnectToSQL()
-//	if err != nil {
-//		log.Fatalf("FATAL, Can't Connect to database")
-//	}
-//	defer db.Close()
-//	s := stores.New(db)
-//	h := handlers.New(s)
-//
-//	r := mux.NewRouter()
-//	r.HandleFunc("/customer/{id}", h.GetByID).Methods(http.MethodGet)
-//	r.HandleFunc("/customer", h.Create).Methods(http.MethodPost)
-//	r.HandleFunc("/customer/delete/{id}", h.DeleteByID).Methods(http.MethodDelete)
-//	r.HandleFunc("/customer/update/{id}", h.UpdateByID).Methods(http.MethodPut)
-//
-//	r.Use(middleware.SetContentType)
-//
-//	err = http.ListenAndServe(":8080", r)
-//	if err != nil {
-//		log.Println("Cant Connect!")
-//	}
-//}
-
 func main() {
 	app := gofr.New()
 	s := stores.New()
